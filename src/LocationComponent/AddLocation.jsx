5c8b2d7d265c351ec7161c8fb5f13be1e8bcfae8
PrashantSingrole <154450036+PrashantSingrole@users.noreply.github.com>
2025-03-29 09:03:53 +0530

    Update AddLocation.jsx

--- src/LocationComponent/AddLocation.jsx
+++ src/LocationComponent/AddLocation.jsx
@@ -12,7 +12,7 @@ const AddLocation = () => {
     e.preventDefault();
     let data = { city, description };
 
-    fetch("http://localhost:8080/api/salon/location/add", {
+    fetch("http://https://scissor-servers.onrender.com/api/salon/location/add", {
       method: "POST",
       headers: {
         Accept: "application/json",
