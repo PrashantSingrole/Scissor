44102f5d02c0b479d41a167a7db693bd5b6a742d
PrashantSingrole <154450036+PrashantSingrole@users.noreply.github.com>
2025-03-29 09:12:30 +0530

    Update MyWallet.jsx

--- src/UserComponent/MyWallet.jsx
+++ src/UserComponent/MyWallet.jsx
@@ -25,7 +25,7 @@ const MyWallet = () => {
 
   const retrieveMyWallet = async () => {
     const response = await axios.get(
-      "http://localhost:8080/api/user/id?userId=" + salon.id
+      "http://https://scissor-servers.onrender.com/api/user/id?userId=" + salon.id
     );
 
     return response.data;
